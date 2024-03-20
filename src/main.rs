988ec3d94637198a4c69738df77a2ed5980ac236
Hannes Saffrich <saffrich@informatik.uni-freiburg.de>
2024-03-21 00:37:56 +0100

    Added example for error reporting with ariadne.

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ pub mod syntax;
 pub mod to_str;
 
 use args::Args;
+use ariadne::{Color, ColorGenerator, Fmt, IndexType, Label, Report, ReportKind, Source};
 use clap::Parser;
 
 use crate::pretty::{pretty, Pretty, PrettyOpts};
@@ -45,6 +46,38 @@ fn main() {
         max_line_len: 80,
     };
     println!("{}", pretty(&p_opts, e));
+
+    let mut colors = ColorGenerator::new();
+
+    // Generate & choose some colours for each of our elements
+    let a = colors.next();
+    let b = colors.next();
+    let out = Color::Fixed(81);
+
+    let src_path = args.src_path.to_string_lossy();
+
+    Report::build(ReportKind::Error, &src_path, 12)
+        .with_config(ariadne::Config::default().with_index_type(IndexType::Byte))
+        .with_code(3)
+        .with_message(format!("Incompatible types"))
+        .with_label(
+            Label::new((&src_path, 2..3))
+                .with_message(format!("This is of type {}", "Nat".fg(a)))
+                .with_color(a),
+        )
+        .with_label(
+            Label::new((&src_path, 12..13))
+                .with_message(format!("This is of type {}", "Str".fg(b)))
+                .with_color(b),
+        )
+        .with_note(format!(
+            "Outputs of {} expressions must coerce to the same type",
+            "match".fg(out)
+        ))
+        .finish()
+        .print((&src_path, Source::from(&src)))
+        .unwrap();
+
     // if let Err(e) = tui::run(&args) {
     //     println!("ERROR: {}", e);
     // }
