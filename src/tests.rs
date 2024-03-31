d8c20d4d4869805d0151782f502af68ffc314cbf
Hannes Saffrich <saffrich@informatik.uni-freiburg.de>
2024-03-31 17:01:08 +0200

    Refactored tests.

--- src/tests.rs
+++ src/tests.rs
@@ -4,18 +4,6 @@ use crate::{
     typecheck,
 };
 
-#[test]
-fn id_unr() {
-    let src = "λ[unr] x. x : Unit -[ unr; pure ]→ Unit";
-    assert!(typecheck(src).is_ok());
-}
-
-#[test]
-fn res_simple() {
-    let src = "close (! 'x' (new {x})) : Unit";
-    assert!(typecheck(src).is_ok());
-}
-
 pub fn typecheck_(src: &str) -> Result<(Type, Eff), IErr> {
     typecheck(src).map_err(|e| {
         report_error("<nofile>", &src, e.clone());
@@ -24,8 +12,28 @@ pub fn typecheck_(src: &str) -> Result<(Type, Eff), IErr> {
 }
 
 #[test]
-fn res_let() {
+fn ty_id_unr_ann() {
+    let src = "λ[unr] x. x : Unit -[ unr; pure ]→ Unit";
+    // let src = "λ[unr] x. x : Unit -[u0]→ Unit";
+    // let src = "λ[unr] x. x : Unit -[u;0]→ Unit";
+    // let src = "λ[unr] x. x : Unit →u0 Unit";
+    assert!(typecheck_(src).is_ok());
+}
+
+#[test]
+fn ty_id_unr() {
+    let src = "λx. x : Unit -[ unr; pure ]→ Unit";
+    assert!(typecheck_(src).is_ok());
+}
+
+#[test]
+fn ty_res_simple() {
+    let src = "close (! 'x' (new {x})) : Unit";
+    assert!(typecheck_(src).is_ok());
+}
+
+#[test]
+fn ty_res_let() {
     let src = "let x, y = split {a} (new {ab}) in close (!'a' x); close (!'b' y) : Unit";
-    let res = typecheck_(src);
-    assert!(res.is_ok(), "Typechecking failed! Error: {res:?}");
+    assert!(typecheck_(src).is_ok());
 }
